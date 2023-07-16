bd53efd65e9fd518572fba40762cf646fdbc74e7
Ravi <ravi@vytec.com>
2023-07-16 21:19:00 +0530

    updated

--- cfn.sh
+++ cfn.sh
@@ -1,5 +1,5 @@
 #aws cloudformation create-stack --stack-name myteststack --template-body file://sampletemplate.json --parameters ParameterKey=KeyPairName,ParameterValue=TestKey ParameterKey=SubnetIDs,ParameterValue=SubnetID1\\,SubnetID2
 
-aws cloudformation update-stack --stack-name demo --TemplateBody file:///$PWD/ec2.yml --parameters ParameterKey=InstanceTypeParameter,ParameterValue=t2.medium
+aws cloudformation update-stack --stack-name demo --template-body file:///$PWD/ec2.yml --parameters ParameterKey=InstanceTypeParameter,ParameterValue=t2.medium
 
 #aws cloudformation delete-stack --stack-name demo
