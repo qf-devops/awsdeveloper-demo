5c62ec9c754cdbb587fd2a1687b2938bac7f8ae9
Ravi <ravi@vytec.com>
2023-07-22 20:33:32 +0530

    upated

--- cfn.sh
+++ cfn.sh
@@ -2,4 +2,7 @@
 
 #aws cloudformation update-stack --stack-name demo --template-body file:///$PWD/ec2.yml --parameters ParameterKey=InstanceTypeParameter,ParameterValue=t2.small
 
-aws cloudformation delete-stack --stack-name demo
+#aws cloudformation delete-stack --stack-name demo
+
+aws cloudformation create-stack --stack-name myecsstack --template-body file:///$PWD/ecs.yml --parameters ParameterKey=SubnetID,ParameterValue=subnet-e58fafa9
+
